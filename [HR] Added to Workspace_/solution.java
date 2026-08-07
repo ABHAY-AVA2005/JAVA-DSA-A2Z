4bf1d413022ab59a827fab972da8a6fff1f18ad0
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-08-07 19:09:50 +0530

    Sync via AlgoSync

---
+++ [HR] Added to Workspace_/solution.java
@@ -0,0 +1,30 @@
+        
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ' || ch == '/') {
+                return 0;
+            }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+            if (Character.isUpperCase(ch)) {
+                hasCapital = true;
+            }
+        }
+        
+        if (hasDigit && hasCapital) {
+            return 1;
+        } else {
+        boolean hasCapital = false;
+        boolean hasDigit = false;
+        
+        }
+            return 0;
+        if (Character.isDigit(str.charAt(0))) {
+        }
+            return 0;
+        if (n < 4) {
+    public static int checkPassword(String str, int n) {
+            return 0;
+        }
+    }
